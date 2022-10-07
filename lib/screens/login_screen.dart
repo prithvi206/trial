6288a86937072e5044d2d08d4a4d5f74bb28d174
Sammy131 <114558761+Sammy131@users.noreply.github.com>
2022-10-07 18:10:20 +0530

    Add files via upload

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,349 @@
+// ignore_for_file: constant_identifier_names, prefer_const_literals_to_create_immutables, prefer_const_constructors, non_constant_identifier_names, avoid_types_as_parameter_names
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:jsskos_app/screens/bottom_nav.dart';
+
+enum MobileVerficationState {
+  SHOW_MOBILE_STATE_STATE,
+  SHOW_OTP_FORM_STATE,
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  MobileVerficationState currentState =
+      MobileVerficationState.SHOW_MOBILE_STATE_STATE;
+
+  final phoneController = TextEditingController();
+  final otpController = TextEditingController();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  late String verificationId;
+
+  bool showLoading = false;
+
+  void signInWithPhoneAuthCredential(
+      PhoneAuthCredential phoneAuthCredential) async {
+    try {
+      final AuthCredential =
+          await _auth.signInWithCredential(phoneAuthCredential);
+      setState(() {
+        showLoading = false;
+      });
+
+      if (AuthCredential?.user != null) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MyBottomNav()));
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        showLoading = false;
+      });
+      final snackBar = SnackBar(
+        content: Text(e.message.toString()),
+        backgroundColor: (Colors.black12),
+        action: SnackBarAction(
+          label: 'dismiss',
+          onPressed: () {},
+        ),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
+  getMobileFormWidget(context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            margin: null,
+            padding: const EdgeInsets.only(left: 9.0, top: 7.0),
+            height: 250,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 214, 84, 67),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: 100,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                  child: Center(
+                    child: Text(
+                      'LOGO',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'WELCOME TO JSSKOU APP',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'LOGIN',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[350],
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25.0, top: 7.0),
+                child: TextField(
+                  controller: phoneController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Enter the Mobile Number",
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Container(
+            width: double.infinity,
+            height: 50,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+            child: TextButton(
+              onPressed: () async {
+                setState(() {
+                  showLoading = true;
+                });
+
+                await _auth.verifyPhoneNumber(
+                  phoneNumber: phoneController.text,
+                  verificationCompleted: (PhoneAuthCredential) async {
+                    setState(() {
+                      showLoading = false;
+                    });
+                    // signInWithPhoneAuthCredential(phoneAuthCredential);
+                  },
+                  verificationFailed: (verificationFailed) async {
+                    //recheck if error
+                    setState(() {
+                      showLoading = false;
+                    });
+                    final snackBar = SnackBar(
+                      content: Text(verificationFailed.message.toString()),
+                      backgroundColor: (Colors.black12),
+                      action: SnackBarAction(
+                        label: 'dismiss',
+                        onPressed: () {},
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  codeSent: (verificationId, forceResendingToken) {
+                    setState(() {
+                      showLoading = false;
+                      currentState = MobileVerficationState.SHOW_OTP_FORM_STATE;
+                      this.verificationId = verificationId;
+                    });
+                  },
+                  codeAutoRetrievalTimeout: ((verificationId) {}),
+                );
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text(
+                "SEND OTP",
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Center(
+              child: Text(
+            'Or',
+            style: TextStyle(fontSize: 25.0),
+          )),
+          SizedBox(
+            height: 60.0,
+          ),
+          Text(
+            'Register',
+            style: TextStyle(
+                fontSize: 35,
+                color: Color.fromARGB(255, 19, 100, 165),
+                decoration: TextDecoration.underline),
+          ),
+        ],
+      ),
+    );
+  }
+
+  getOtpFormWidget(context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 9.0, top: 7.0),
+            height: 250,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 214, 84, 67),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: 100,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                  child: Center(
+                    child: Text(
+                      'LOGO',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'WELCOME TO JSSKOU APP',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 55.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(12)),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25.0, top: 7.0),
+                child: TextField(
+                  controller: otpController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Enter the OTP",
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Container(
+            height: 50,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+            width: double.infinity,
+            child: TextButton(
+              onPressed: () async {
+                PhoneAuthCredential phoneAuthCredential =
+                    PhoneAuthProvider.credential(
+                        verificationId: verificationId,
+                        smsCode: otpController.text);
+
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text(
+                "Verify",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Center(
+              child: Text(
+            'Or',
+            style: TextStyle(fontSize: 25.0),
+          )),
+          SizedBox(
+            height: 60.0,
+          ),
+          Text(
+            'Register',
+            style: TextStyle(
+                fontSize: 35,
+                color: Color.fromARGB(255, 19, 100, 165),
+                decoration: TextDecoration.underline),
+          ),
+        ],
+      ),
+    );
+  }
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Container(
+        child: showLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : currentState == MobileVerficationState.SHOW_MOBILE_STATE_STATE
+                ? getMobileFormWidget(context)
+                : getOtpFormWidget(context),
+      ),
+    );
+  }
+}
